69f16fb7d1762d0c2989394f8d386597d6004c0a
shunsuke satou <shunsuke.satou@gmail.com>
2025-02-19 21:13:19 +0900

    Pre.3

--- Dto/ReleaseNote.cs
+++ Dto/ReleaseNote.cs
@@ -16,7 +16,8 @@ public class ReleaseNote
     {
         // Versionは単純に整数でカウントアップする
         var releaseNotes = new ReleaseNote[] {
-            new ReleaseNote() {Version = "Pre.2" , Note = "楽曲詳細の表示を変更", ReleaseDate = new DateTime(2021, 2, 11), },
+            new ReleaseNote() {Version = "Pre.3" , Note = "card styleを全体に適用", ReleaseDate = new DateTime(2025, 2, 19), },
+            new ReleaseNote() {Version = "Pre.2" , Note = "楽曲詳細の表示を変更", ReleaseDate = new DateTime(2025, 2, 11), },
             new ReleaseNote() { Version = "Pre.1" ,Note = "プレリリース", ReleaseDate = new DateTime(2025,2, 9), },
     };
 
