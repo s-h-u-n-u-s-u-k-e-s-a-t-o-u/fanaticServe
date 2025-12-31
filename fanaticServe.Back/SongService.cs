ce409546cb0e20b4a04a6539f0394841e0a8e2ab
shunsuke satou <shunsuke.satou@gmail.com>
2025-12-31 18:57:01 +0900

    マージ実施

--- fanaticServe.Back/SongService.cs
+++ fanaticServe.Back/SongService.cs
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-﻿using fanaticServe.Back;
-using fanaticServe.Core.Data;
-using Microsoft.AspNetCore.Mvc;
-
-namespace fanaticServe.Controllers;
-
-public class SongsController : Controller
-{
-    private readonly IFanaticServeContext _context;
-
-    public SongsController(IFanaticServeContext context)
-    {
-        _context = context;
-    }
-
-    [HttpGet]
-    public IActionResult Index(string sortOrder, string searchString)
-    {
-        ViewData["TitleSort"] = String.IsNullOrEmpty(sortOrder) ? "Title_desc" : "";
-        ViewData["CountSort"] = sortOrder == "Count" ? "Count_desc" : "Count";
-
-        ViewData["CurrentSort"] = sortOrder;
-        ViewData["CurrentFilter"] = searchString;
-
-        return View(new SongService(_context).GetAllSongs(sortOrder, searchString));
-    }
-
-    [HttpGet]
-    public IActionResult Detail(Guid? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        return View(new SongService(_context).GetSong(id.Value));
-=======
 ﻿using fanaticServe.Core.Data;
 using fanaticServe.Core.Dto;
 
@@ -164,6 +126,5 @@ public class SongService : ISongs
             ).ToArray();
 
         return song;
->>>>>>> ディレクトリ構成変更
     }
 }
