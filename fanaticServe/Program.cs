91084f521fa76915d552e023c9b9258c5d06fa89
shunsuke satou <shunsuke.satou@gmail.com>
2026-05-16 21:54:18 +0900

    DBをDatasourceとする

--- fanaticServe/Program.cs
+++ fanaticServe/Program.cs
@@ -1,28 +1,38 @@
 // Import the Azure.Monitor.OpenTelemetry.AspNetCore namespace.
+
 using fanaticServe.Core.Data;
 using fanaticServe.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // ログの設定
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
-//builder.Logging.AddDebug();
+builder.Logging.AddDebug();
 
 // Add OpenTelemetry and configure it to use Azure Monitor.
 // APPLICATIONINSIGHTS_CONNECTION_STRING という名前で環境変数を設定し、UseAzureMonitor()を呼び出せば自動的に認識されます。
 // builder.Services.AddOpenTelemetry().UseAzureMonitor();
 
 // Add services to the container.
+
+builder.Configuration.AddEnvironmentVariables(prefix: "SQLCONNSTR_SSSWare_");
+var connectionString = Environment.GetEnvironmentVariable("SQLCONNSTR_SSSWare_ConnectionStrings__DefaultConnection");
+
+builder.Services.AddDbContext<fanaticServe.Data.FanaticServeContext>(options =>
+    options.UseSqlServer(connectionString, providerOptions => providerOptions.EnableRetryOnFailure()));
+
+
+// インターフェイスを DbContext にマップする
+builder.Services.AddScoped<IFanaticServeContext>(provider => provider.GetRequiredService<FanaticServeContext>());
+
+
+builder.Services.AddAuthorization();
 builder.Services.AddControllersWithViews();
 
-// Database を使わない -> DbContext 登録を削除し、ファイルコンテキストを登録する
-// 既定では ContentRoot/DataSource/*.dat を読み込みます
-builder.Services.AddSingleton<IFanaticServeContext>(sp =>
-    new FanaticServeFileContext(builder.Environment.ContentRootPath));
-
-// データベース例外フィルターは不要なのでコメントアウト（必要なら別処理）
-// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+// データベース例外フィルター
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
