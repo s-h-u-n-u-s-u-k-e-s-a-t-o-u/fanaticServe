ed2a1237a70e269b143f2500751dcc9b70b1c19f
shunsuke satou <shunsuke.satou@gmail.com>
2026-06-18 23:50:34 +0900

    星取表実装

---
+++ fanaticServe/Controllers/StarMatrixController.cs
@@ -0,0 +1,21 @@
+﻿using fanaticServe.Back;
+using fanaticServe.Core.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fanaticServe.Controllers;
+
+public class StarMatrixController : Controller
+{
+    private readonly IFanaticServeContext _context;
+
+    public StarMatrixController(IFanaticServeContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var service = new StarMatrixService(_context);
+        return View(service.GetStarMatrix());
+    }
+}
