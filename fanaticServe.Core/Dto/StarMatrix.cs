dfbcf261ef15a21798dd2607098607919378d5b3
佐藤俊輔 <159138891+s-h-u-n-u-s-u-k-e-s-a-t-o-u@users.noreply.github.com>
2026-06-19 21:30:47 +0900

    Feature/star matrix (#76)
    * ALL AIKATSU
    
    * 星取表実装
    
    * 実装完了

---
+++ fanaticServe.Core/Dto/StarMatrix.cs
@@ -0,0 +1,29 @@
+﻿using fanaticServe.Core.Models;
+
+namespace fanaticServe.Core.Dto;
+
+public class StarMatrix
+{
+    public int rowCount { get; set; }
+    public int columnCount { get; set; }
+    public required StarMatrixHeader[] Header { get; set; }
+    public required StarMatrixRowHeader[] RowHeader { get; set; }
+    public required bool[,] Cells { get; set; } 
+}
+
+public class StarMatrixHeader
+{
+    public required Guid SongId { get; set; }
+    public required string Title { get; set; }
+    public DateTime ReleaseOn { get; set; }
+    public int Track_No { get; set; }
+}
+
+public class StarMatrixRowHeader
+{
+    public required Guid Live_Event_Id { get; set; }
+    public required string Title { get; set; }
+    public DateTime Perform_At { get; set; }
+    public required List<Set_list> SetLists { get; set; }
+}
+
---
+++ fanaticServe/Controllers/StarMatrixController.cs
@@ -0,0 +1,28 @@
+﻿using fanaticServe.Back;
+using fanaticServe.Core.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fanaticServe.Controllers;
+
+public class StarMatrixController : Controller
+{
+    private readonly IFanaticServeContext _context;
+
+    public StarMatrixController(IFanaticServeContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var service = new StarMatrixService(_context);
+        return View(service.GetStarMatrix());
+    }
+
+    // Modalを表示するパーシャルビューを返すアクション
+    public IActionResult GetModal()
+    {
+        var service = new StarMatrixService(_context);
+        return PartialView("_StarMatrixModal", service.GetStarMatrix());
+    }
+}
